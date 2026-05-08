aead001fbdc2b7da6d20cd2cc11f934157e6cdcd
nab2914 <nandithaannp@gmail.com>
2026-05-09 01:06:15 +0530

    Remove sorting and redistribution logic from mergeArrays and accompanying example code.

--- sort.c
+++ sort.c
@@ -12,24 +12,3 @@ def mergeArrays(arr1, arr2):
         merged[i] = arr1[i]
     for j in range(m):
         merged[n + j] = arr2[j]
-
-    # sort the merged array
-    merged.sort()
-
-    # distribute first n elements to arr1
-    for i in range(n):
-        arr1[i] = merged[i]
-
-    # distribute remaining m elements to arr2
-    for j in range(m):
-        arr2[j] = merged[n + j]
-
-
-if __name__ == "__main__":
-    arr1 = [1, 3, 5, 7]
-    arr2 = [2, 4, 6, 8]
-
-    mergeArrays(arr1, arr2)
-
-    print(*arr1)
-    print(*arr2)
